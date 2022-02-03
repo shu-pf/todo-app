bbbb7a59bef6a5c25edaddb86f757993735fc3b3
Shugo Furuse <shugoena@gmail.com>
2022-02-03 18:33:36 +0900

    feat: Reset CSSの導入

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Global, css } from '@emotion/react';
+import restCss from 'ress';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css(`
+          ${restCss}
+          body {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+        `)}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
