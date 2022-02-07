2dd793eb8280b13ca0223f932e134bc68d0f195d
Shugo Furuse <shugoena@gmail.com>
2022-02-07 11:16:01 +0900

    fix: StoryBookでNextのコンポーネントが読み込まれないため、追加の設定を定義

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,13 @@ import { ThemeProvider } from '@emotion/react';
 import { defaultTheme } from '../themes/default';
 import { Global } from '@emotion/react';
 import { GlobalStyle } from '../styles/globals';
+import * as NextImage from 'next/image';
+
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
