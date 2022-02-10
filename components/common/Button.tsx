f169988f0ccb271a0a2393de9a576d32d2b83113
Shugo Furuse <shugoena@gmail.com>
2022-02-10 14:19:54 +0900

    feat: buttonコンポーネントの実装

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,25 +1,91 @@
 /** @jsxImportSource @emotion/react */
-import { useTheme } from '@emotion/react';
+import { css, SerializedStyles, Theme, useTheme } from '@emotion/react';
+import { useEffect, useState } from 'react';
 
 interface ButtonProps {
   label: string;
+  variant?: 'primary' | 'error' | 'outlined' | 'underlined';
+  shadow?: boolean;
+  disabled?: boolean;
   onClick?: () => void;
 }
 
-export const Button = ({ label, ...props }: ButtonProps) => {
+const baseBtnStyle = css`
+  padding: 10px;
+  border-radius: 5px;
+  transition: 0.2s;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+function btnStyles(theme: Theme) {
+  return {
+    primary: [
+      baseBtnStyle,
+      css`
+        background-color: ${theme.colors.primary.green};
+        color: ${theme.colors.text.navy};
+      `,
+    ],
+    error: [
+      baseBtnStyle,
+      css`
+        background-color: ${theme.colors.primary.pink};
+        color: ${theme.colors.text.white};
+      `,
+    ],
+    outlined: [
+      baseBtnStyle,
+      css`
+        border: 1px solid ${theme.colors.component.gray};
+        color: ${theme.colors.text.gray};
+      `,
+    ],
+    underlined: [
+      baseBtnStyle,
+      css`
+        text-decoration: underline;
+        color: ${theme.colors.component.pureWhite};
+      `,
+    ],
+  };
+}
+
+const btnShadowStyle = (theme: Theme) => css`
+  box-shadow: 0px 0px 13px ${theme.colors.component.shadowBlack};
+`;
+
+const disabledBtnStyles = (theme: Theme) => [
+  baseBtnStyle,
+  css`
+    color: ${theme.colors.component.gray};
+    background: ${theme.colors.component.lighterLightGray};
+  `,
+];
+
+export const Button = ({ label, shadow, variant = 'primary', ...props }: ButtonProps) => {
   const theme = useTheme();
+
+  const [buttonStyles, setButtonStyles] = useState<SerializedStyles[]>();
+
+  useEffect(() => {
+    if (props.disabled) {
+      setButtonStyles(disabledBtnStyles(theme));
+      return;
+    }
+
+    const styles = btnStyles(theme)[variant];
+
+    if (shadow) {
+      styles.push(btnShadowStyle(theme));
+    }
+
+    setButtonStyles(styles);
+  }, [shadow, theme, variant, props.disabled]);
+
   return (
-    <button
-      css={{
-        backgroundColor: theme.colors.primary.green,
-        color: theme.colors.text.navy,
-        borderRadius: '5px',
-        fontSize: '14px',
-        padding: '8px',
-      }}
-      type="button"
-      {...props}
-    >
+    <button css={buttonStyles} type="button" {...props}>
       {label}
     </button>
   );
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Button } from '../components/common/Button';
 
 const meta: ComponentMeta<typeof Button> = {
-  title: 'Button',
+  title: 'component/common/Button',
   component: Button,
 };
 
@@ -15,4 +15,25 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   label: 'Button',
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  label: 'Button',
+  variant: 'error',
+};
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+  label: 'Button',
+  variant: 'outlined',
+};
+
+export const Underlined = Template.bind({});
+Underlined.args = {
+  label: 'Button',
+  variant: 'underlined',
+};
+Underlined.parameters = {
+  backgrounds: { default: 'dark' },
 };
