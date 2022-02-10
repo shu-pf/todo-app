90c1c1294c296d14e3cf0a26486f69700ff68a8e
Shugo Furuse <shugoena@gmail.com>
2022-02-10 18:32:28 +0900

    feat: アイコンの追加

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,12 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { css, SerializedStyles, Theme, useTheme } from '@emotion/react';
 import { useEffect, useState } from 'react';
-
+import Add from './icons/add.svg';
 interface ButtonProps {
   label: string;
   variant?: 'primary' | 'error' | 'outlined' | 'underlined';
   shadow?: boolean;
   disabled?: boolean;
+  icon?: boolean;
   onClick?: () => void;
 }
 
@@ -21,34 +22,58 @@ const baseBtnStyle = css`
 
 function btnStyles(theme: Theme) {
   return {
-    primary: [
-      baseBtnStyle,
-      css`
-        background-color: ${theme.colors.primary.green};
-        color: ${theme.colors.text.navy};
+    primary: {
+      btn: [
+        css`
+          background-color: ${theme.colors.primary.green};
+          color: ${theme.colors.text.navy};
+        `,
+      ],
+      icon: css`
+        path {
+          fill: ${theme.colors.text.navy};
+        }
       `,
-    ],
-    error: [
-      baseBtnStyle,
-      css`
-        background-color: ${theme.colors.primary.pink};
-        color: ${theme.colors.text.white};
+    },
+    error: {
+      btn: [
+        css`
+          background-color: ${theme.colors.primary.pink};
+          color: ${theme.colors.text.white};
+        `,
+      ],
+      icon: css`
+        path {
+          fill: ${theme.colors.text.white};
+        }
       `,
-    ],
-    outlined: [
-      baseBtnStyle,
-      css`
-        border: 1px solid ${theme.colors.component.gray};
-        color: ${theme.colors.text.gray};
+    },
+    outlined: {
+      btn: [
+        css`
+          border: 1px solid ${theme.colors.component.gray};
+          color: ${theme.colors.text.gray};
+        `,
+      ],
+      icon: css`
+        path {
+          fill: ${theme.colors.text.gray};
+        }
       `,
-    ],
-    underlined: [
-      baseBtnStyle,
-      css`
-        text-decoration: underline;
-        color: ${theme.colors.component.pureWhite};
+    },
+    underlined: {
+      btn: [
+        css`
+          text-decoration: underline;
+          color: ${theme.colors.component.pureWhite};
+        `,
+      ],
+      icon: css`
+        path {
+          fill: ${theme.colors.component.pureWhite};
+        }
       `,
-    ],
+    },
   };
 }
 
@@ -56,21 +81,38 @@ const btnShadowStyle = (theme: Theme) => css`
   box-shadow: 0px 0px 13px ${theme.colors.component.shadowBlack};
 `;
 
-const disabledBtnStyles = (theme: Theme) => [
-  baseBtnStyle,
-  css`
-    color: ${theme.colors.component.gray};
-    background: ${theme.colors.component.lighterLightGray};
-  `,
-];
+const disabledBtnStyles = (theme: Theme) => {
+  return {
+    btn: [
+      css`
+        color: ${theme.colors.component.gray};
+        background: ${theme.colors.component.lighterLightGray};
+        cursor: default;
+      `,
+    ],
+    icon: css`
+      path {
+        fill: ${theme.colors.component.gray};
+      }
+    `,
+  };
+};
 
-export const Button = ({ label, shadow, variant = 'primary', ...props }: ButtonProps) => {
+export const Button = ({
+  label,
+  shadow,
+  variant = 'primary',
+  disabled,
+  icon = false,
+  ...props
+}: ButtonProps) => {
   const theme = useTheme();
 
-  const [buttonStyles, setButtonStyles] = useState<SerializedStyles[]>();
+  const [buttonStyles, setButtonStyles] =
+    useState<{ btn: SerializedStyles[]; icon: SerializedStyles }>();
 
   useEffect(() => {
-    if (props.disabled) {
+    if (disabled) {
       setButtonStyles(disabledBtnStyles(theme));
       return;
     }
@@ -78,14 +120,24 @@ export const Button = ({ label, shadow, variant = 'primary', ...props }: ButtonP
     const styles = btnStyles(theme)[variant];
 
     if (shadow) {
-      styles.push(btnShadowStyle(theme));
+      styles.btn.push(btnShadowStyle(theme));
     }
 
     setButtonStyles(styles);
-  }, [shadow, theme, variant, props.disabled]);
+  }, [shadow, theme, variant, disabled]);
 
   return (
-    <button css={buttonStyles} type="button" {...props}>
+    <button css={[buttonStyles?.btn, baseBtnStyle]} type="button" disabled {...props}>
+      {icon && (
+        <Add
+          css={[
+            buttonStyles?.icon,
+            css`
+              margin-right: 4px;
+            `,
+          ]}
+        />
+      )}
       {label}
     </button>
   );
