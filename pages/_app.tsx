2eae7a8a885867cfd9794c2bffdbab9f0471a71f
Shugo Furuse <shugoena@gmail.com>
2022-02-14 15:14:13 +0900

    feat: ファビコンを適用

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import { Global } from '@emotion/react';
 import { ThemeProvider } from '@emotion/react';
 import { defaultTheme } from '../themes/default';
 type DefaultTheme = typeof defaultTheme;
-
+import Head from 'next/head';
 declare module '@emotion/react' {
   // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface Theme extends DefaultTheme {}
@@ -13,6 +13,16 @@ declare module '@emotion/react' {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="description" content="コレをマスターすれば天才エンジニア？高沼カリキュラム" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <Global styles={GlobalStyle} />
       <ThemeProvider theme={defaultTheme}>
         <Component {...pageProps} />
