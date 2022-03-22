4f3663827345afb6294997e40091052b3d59887a
shu-pf <s_furuse@yumemi.co.jp>
2022-03-22 20:32:23 +0900

    fix: JSON.parseで失敗した場合の処理を追加

--- data/libs/title-serializer.ts
+++ data/libs/title-serializer.ts
@@ -5,7 +5,11 @@ interface DeserializedTitle {
 }
 
 export const titleDeserializer = (title: string): DeserializedTitle => {
-  return JSON.parse(title);
+  try {
+    return JSON.parse(title);
+  } catch {
+    return { title: '不明なタイトル', checked: false, detail: '' };
+  }
 };
 
 export const titleSerializer = ({ title, checked, detail }: DeserializedTitle): string => {
